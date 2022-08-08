b207feeeabf935cb87f1e6a10ee063def33f52d4
Nídio Dolfini <nidiosdolfini@gmail.com>
2022-08-08 14:40:47 -0300

    Estrutura do projeto

---
+++ src/main/java/br/edu/infnet/appcalorias/AppcaloriasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appcalorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcaloriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcaloriasApplication.class, args);
+	}
+
+}
